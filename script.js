153cf9fa7f797f05c49b1d25484e160fcb9cfd4e
2ikki <90100920+2ikki@users.noreply.github.com>
2021-09-05 17:55:09 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ $(function(){
         
     $(this).parent('.card').toggleClass('open');
     $(this).toggleClass('img_open');
-    $(this).parent('.card').children('p').slideToggle(400)
 
     })
 })
